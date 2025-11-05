8a1559cf4a265c5cf7832c8912802c532b66ffa3
siya09568-ux <siya09568@gmail.com>
2025-11-05 14:31:16 +0530

    Q27

---
+++ DAY14/Q27.c
@@ -0,0 +1,12 @@
+//Write a program to print the sum of the first n odd numbers.
+#include <stdio.h>
+int main(){
+    int n, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++) {
+        sum += 2 * i - 1;
+    }
+    printf("Sum of the first %d odd numbers is: %d\n", n, sum);
+    return 0;
+}

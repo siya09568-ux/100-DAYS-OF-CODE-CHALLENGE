c056465ecf8cdf3aedb028f28708923f548e91ea
siya09568-ux <siya09568@gmail.com>
2025-11-05 20:59:35 +0530

    Q47

---
+++ DAY24/Q47.c
@@ -0,0 +1,19 @@
+// Write a program to print the following pattern:
+// *
+// **
+// ***
+// ****
+// *****
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter Number of rows: ");
+    scanf("%d",&n);
+    for (int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+}

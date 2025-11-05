7023d74eff310c1422c4d6a0b361f4848ee16d7a
siya09568-ux <siya09568@gmail.com>
2025-11-05 15:02:17 +0530

    Q36

---
+++ DAY18/Q36.c
@@ -0,0 +1,13 @@
+//Write a program to find the HCF (GCD) of two numbers.
+#include <stdio.h>
+int main() {
+    int num1, num2, hcf;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    for(int i = 1; i <= num1 || i <= num2; i++) {
+        if (num1 % i == 0 && num2 % i == 0)
+            hcf = i;
+    }
+    printf("HCF of %d and %d is %d\n", num1, num2, hcf);
+    return 0;
+}

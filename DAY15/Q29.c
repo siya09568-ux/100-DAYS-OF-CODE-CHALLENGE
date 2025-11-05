75cea625f86b701f3459d4228643226612beb9cf
siya09568-ux <siya09568@gmail.com>
2025-11-05 14:34:16 +0530

    Q29

---
+++ DAY15/Q29.c
@@ -0,0 +1,13 @@
+//Write a program to calculate the factorial of a number.
+
+#include <stdio.h>
+int main(){
+    int n, factorial = 1;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+    printf("Factorial of %d is: %d\n", n, factorial);
+    return 0;
+}

bbda7c671abaff7904e07f474757bbd5a87c10bf
siya09568-ux <siya09568@gmail.com>
2025-11-05 20:49:52 +0530

    Q26

---
+++ DAY23/Q26.c
@@ -0,0 +1,19 @@
+// Write a program to print the following pattern:
+// *****
+// *****
+// *****
+// *****
+// *****
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Enter the Number: ");
+    scanf("%d",&n);
+    for (int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+}

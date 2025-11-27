4fd5e3dbbf2c0fce77bfd94e1f1a9ffc2dd8b5de
siya09568-ux <siya09568@gmail.com>
2025-11-27 16:02:34 +0530

    Create Q135.c

---
+++ DAY85/Q135.c
@@ -0,0 +1,7 @@
+/* Assign explicit values starting from 10 and print them.*/
+#include <stdio.h>
+enum Values { A = 10, B, C, D };
+int main(){
+    printf("%d %d %d %d\n", A, B, C, D);
+    return 0;
+}

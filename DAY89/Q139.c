7f757cf3a44faebc73ba718c0e796adac994e3be
siya09568-ux <siya09568@gmail.com>
2025-11-27 16:10:33 +0530

    Create Q139.c

---
+++ DAY89/Q139.c
@@ -0,0 +1,8 @@
+/*
+Show that enums store integers by printing assigned values.*/
+#include <stdio.h>
+enum test {X=5, Y=10, Z=15};
+int main(){
+    printf("X=%d, Y=%d, Z=%d\n", X, Y, Z);
+    return 0;
+}
